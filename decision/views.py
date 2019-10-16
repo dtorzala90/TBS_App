0c817741edff7e07d47dcd4d4f939a530cb2925c
Daniel Torzala <danieltorzala@gmail.com>
2019-10-15 21:51:14 -0700

    Initial Upload of the basic App

---
+++ decision/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render 
+from .models import Post
+
+
+# Create your views here.
+def home(request):
+	context = {
+		'posts': Post.objects.all()
+	}
+	return render(request, 'decision/home.html', context)
+
+def about(request):
+	return render(request, 'decision/about.html', {'title': 'About'})
