74f1879c8d285fdceff9dca604a7c5661100b660
lizmos1 <emos1@asu.edu>
2019-11-26 11:46:05 -0600

    Completed task #183 under US#129
    I have implemented a javascript file that will be the basis for the
    general alert system. Within this script the system checks every second
    to see if an alert must be thrown based on certain parameters. I also
    added some comments to my code in hope of clarify what I have done.

--- decision/static/js/initTimer.js
+++ decision/static/js/initTimer.js
@@ -1,4 +1,4 @@
-    <!-- Clear data from previous session and initialize global values -->
-        localStorage.clear();
-        localStorage.setItem("ivAlert", "not thrown");
-        localStorage.setItem("IVACCESS", "false");
+<!-- Clear data from previous session and initialize global values -->
+localStorage.clear();
+localStorage.setItem("ivAlert", "not thrown");
+localStorage.setItem("IVACCESS", "false");
