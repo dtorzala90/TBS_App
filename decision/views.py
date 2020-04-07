47adc40cc1dd7952b81b0ab29df35d0fda7d0023
Daniel Torzala <danieltorzala@gmail.com>
2020-04-06 18:39:57 -0700

    Formatted login page to match the rest of the application according to Task#276

--- decision/views.py
+++ decision/views.py
@@ -3,9 +3,32 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
 
 from .models import Session
 from django.core import serializers
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request = request,
+                    template_name = "registration/login.html",
+                    context={"form":form},
+                    attrs={'class': 'myfieldclass'})
 
 # Create your views here.
 @login_required
